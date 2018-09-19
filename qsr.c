d865f443783868b388e7efae2ee853bfb5cef2c0
Thea Birk Berger <BirkBerger@brugers-mbp-4.clients.wireless.dtu.dk>
2018-09-19 16:06:49 +0200

    Created 3 functions: updateParams1(), calculateRR(), updateParams2()

--- qsr.c
+++ qsr.c
@@ -36,18 +36,18 @@ void updateParams2(QSR_params *params) {
     for (int i = 0; i <= 7; i++) {
         sum1 += params->RR_interval1[i];
     }
-    params->Average1 = sum/8;
+    params->RR_average1 = sum1/8;
 
     // Beregner gennemsnit af RR-intervaller fra peaks mellem RR_LOW og RR_HIGH der ikke er fundet via. searchback
     int sum2 = 0;
     for (int i = 0; i <= 7; i++) {
         sum2 += params->RR_interval2[i];
     }
-    params->Average2;
+    params->RR_average2 = sum2/8;
 
-    params->RR_LOW = 0.92 * params->Average2;
-    params->RR_HIGH = 1.19 * params->Average2;
-    params->MISS = 1.66 * params->Average2;
+    params->RR_low = 0.92 * params->RR_average2;
+    params->RR_high = 1.19 * params->RR_average2;
+    params->MISS = 1.66 * params->RR_average2;
     params->THRESHOLD1 = params->NPKF + 0.25 * (params->SPKF - params->NPKF);
     params->THRESHOLD2 = 0.5 * params->THRESHOLD1;
 
