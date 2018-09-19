63335e0344dcf5e489fe667b85e467698a29ddac
Andreas Goericke <andreasgoericke@gmail.com>
2018-09-19 16:47:42 +0200

    merge

--- qsr.h
+++ qsr.h
@@ -15,20 +15,16 @@ typedef struct QSR_params
    int Xp[3];
    int counter;
    int R_peak[1];
-   int RR_interval1[1];
-   int RR_interval2[1];
+   int RR_interval[1];
    int RR_average1;
    int RR_average2;
    int RR_low;
    int RR_high;
    int RR_miss;
-   int SB;
    
 } QSR_params;
 
 // Feel free to change and add methods
 void peakDetection(QSR_params *params);
-int findPeak (QSR_params *params);
-void searchBack(QSR_params *params);
 
 #endif // QSR_H
