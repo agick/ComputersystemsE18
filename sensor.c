c8e519c373cfafe8b50e528617b0b7809a91de5f
Andreas Goericke <andreasgoericke@gmail.com>
2018-09-19 11:06:59 +0200

    First commit

---
+++ sensor.c
@@ -0,0 +1,14 @@
+#include "sensor.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int getNextData(FILE *file)
+{
+   return 0; // return sensor value
+}
+
+FILE* openfile(const char* filename)
+{
+   FILE *file = filename;
+   return file;
+}
