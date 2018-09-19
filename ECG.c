a131a3af00c05362ea166ed053e99c487c4852b1
Andreas Goericke <andreasgoericke@gmail.com>
2018-09-19 11:27:19 +0200

    tilføjet filer ECG.c og filters.c

---
+++ ECG.c
@@ -0,0 +1,85 @@
+/*
+ ============================================================================
+ Name        : ECG.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "sensor.h"
+#include "filters.h"
+#include "qsr.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Main function for organizing the program execution.
+// The functions and object predefined are just for inspiration.
+// Please change orden,names arguments to fit your solution.
+
+int main(void)
+{
+
+    //QRS_params qsr_params;       // Instance of the made avaiable through: #include "qsr.h"
+    FILTERS_params filters_params;
+    filters_params = initFilterParams(&filters_params);
+
+    //OPEN FILE
+	FILE *file;                  // Pointer to a file object
+	file = openfile("ECG.txt");
+
+	printf("%s\t", "Input");
+	printf("%s\t", "LPF");
+	printf("%s\t", "HPF");
+	printf("%s\t", "DRV");
+	printf("%s\t", "SQR");
+	printf("%s\n", "MWI");
+
+	int i = 0;
+	int j = 100;
+	//RUN PROGRAM
+	while(j > 0) {
+		j--;
+		i = getNextData(file);
+		//UPDATE FILTERS_PARAMS
+		filters_params = updateFilterParams(&filters_params);
+		filters_params.X[0] = i;
+
+		//printf("%d\n", filters_params.Y);
+
+
+		lowPassFilter(&filters_params);
+		highPassFilter(&filters_params);
+		derivative(&filters_params);
+		squaring(&filters_params);
+		mwi(&filters_params);
+
+
+
+		printf("%d\t", filters_params.X[0]);
+		printf("%d\t", filters_params.lpfX[0]);
+		printf("%d\t", filters_params.hpfX[0]);
+		printf("%d\t", filters_params.derX);
+		printf("%d\t", filters_params.sqrX[0]);
+		printf("%d\n", filters_params.Y[0]);
+
+	}
+         // Read Data from Sensor
+
+
+
+
+
+    //lowPassFilter();            // Filter Data
+
+
+
+    //peakDetection(&qsr_params); // Perform Peak Detection
+
+
+    puts("hello world");
+	return 0;
+}
+
+
--- filters.h
+++ filters.h
@@ -2,7 +2,34 @@
 #define FILTERS_H
 
 // Feel free to change return statement and arguments
-void lowPassFilter();
+
+typedef struct FILTERS_params
+{ // Structure for QRS parameters
+	int Y[2];
+	int X[13];
+	int lpfX[33];
+	int hpfX[6];
+	int derX;
+	int sqrX[100];
+	int FREQ;
+	int N;
+} FILTERS_params;
+
+FILTERS_params updateFilterParams(FILTERS_params *params);
+FILTERS_params initFilterParams(FILTERS_params *params);
+
+FILTERS_params updateLpfParams(FILTERS_params *params);
+FILTERS_params updateHpfParams(FILTERS_params *params);
+FILTERS_params updateSqrParams(FILTERS_params *params);
+
+void lowPassFilter(FILTERS_params *params);
+void highPassFilter(FILTERS_params *params);
+void derivative(FILTERS_params *params);
+void squaring(FILTERS_params *params);
+void mwi(FILTERS_params *params);
+
+
+
 
 // Implement additional filters
  
