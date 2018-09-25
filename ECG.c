502aa3b5c5ffb436bec44c52c7b27f9ca044d764
Thea Birk Berger <BirkBerger@Brugers-MacBook-Pro-4.local>
2018-09-25 22:47:22 +0200

    --

--- ECG.c
+++ ECG.c
@@ -100,7 +100,7 @@ int main(void)
 
 /* ============================================================================
  * Tjek samhørighed med hvornår der rightShiftes - gennen hele forløbet i figuren
- * 
+ *
  *
  *
  */
